99ffb7267728f08c73a36eaa3d9928867a8af537
michael-tsonkov <mcjaiyo01@gmail.com>
2025-07-11 10:02:12 +0300

    worker exercises complete

--- service-workers/web-worker-exercise/web/js/worker.js
+++ service-workers/web-worker-exercise/web/js/worker.js
@@ -2,13 +2,27 @@
 
 var curFib = 0;
 
-// TODO
-
+self.postMessage("Hello from the web worker!");
 // **********************************
+self.onmessage = onMessage;
+
+function onMessage(event) {
+    getNextFib();
+}
+
+function getNextFib() {
+    var fibNumber = fib(curFib);
+    self.postMessage({
+        fib: fibNumber,
+        index: curFib
+    });
+    curFib++;
+    getNextFib();
+}
 
 function fib(n) {
-	if (n < 2) {
-		return n;
-	}
-	return fib(n-1) + fib(n-2);
+    if (n < 2) {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
 }
